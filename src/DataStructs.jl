177276e8b71502d85b673e26ca55eac422fdd042
João Vitor Toppa <jvtoppa@gmail.com>
2026-02-01 19:57:36 -0300

    fix: format export statement and import base

--- src/DataStructs.jl
+++ src/DataStructs.jl
@@ -2,7 +2,7 @@ module DataStructs
 
 include("stack.jl")
 include("linkedlist.jl")
-export Container, Stack, Queue, Push, Pop, Peek, TopIdx, BottomIdx, SizeC, isEmpty, FindElement, ConstructLL,
-RemoveNode, InsertAnyNode, NodeLL, LinkedList
-
+export Container, Stack, Queue, Push, Pop, Peek, TopIdx, BottomIdx, SizeC, isEmpty, FindElement,
+RemoveNode, InsertAnyNode, NodeLL, LinkedList 
+import Base: show
 end
