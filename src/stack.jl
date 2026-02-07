98d92a469662797ee958883114e9c0b0e5768941
jvtoppa <jvtoppa@gmail.com>
2026-02-07 06:53:21 -0300

    feat: added iterator support

--- src/stack.jl
+++ src/stack.jl
@@ -1,4 +1,6 @@
+module Containers
 #Structs
+export Stack, Queue,  push!, pop!, isempty, peek
 
 abstract type Container{T} end
 
@@ -18,11 +20,13 @@ end
 
 #Constructors
 
+Stack{T}() where T = Stack(T[], 0, 0)
 Stack(T::Type) = Stack(T[], 0, 0)
 Stack() = Stack(Any[], 0, 0)
 Stack(v::Vector{T}) where T = Stack(v, length(v), length(v))
 
 Queue(T::Type) = Queue(T[], 0, 0, 0)
+Queue{T}() where T = Queue(T[], 0, 0, 0)
 Queue() = Queue(Any[], 0, 0, 0)
 Queue(v::Vector{T}) where T = Queue(v, length(v), 0, length(v))
 
@@ -33,16 +37,16 @@ BottomIdx(q::Queue) = q.last
 
 #size()
 
-SizeC(cont::Container) = cont.size
+Base.size(cont::Container) = cont.size
 
-#isEmpty()
+#isempty()
 
-isEmpty(cont::Container) = cont.first == 0
+Base.isempty(cont::Container) = cont.first == 0
 
 #peek()
 
-function Peek(cont::Container)
-    if isEmpty(cont)
+function peek(cont::Container)
+    if isempty(cont)
         error("Container is Empty.")
     else
         return cont.vec[TopIdx(cont)]
@@ -52,14 +56,14 @@ end
 
 #push()
 
-function Push(cont::Container{T}, elem::T) where T
+function Base.push!(cont::Container{T}, elem::T) where T
     if cont isa Stack
         push!(cont.vec, elem)
         cont.first += 1
         cont.size += 1
     elseif cont isa Queue
         pushfirst!(cont.vec, elem)
-        if isEmpty(cont)
+        if isempty(cont)
             cont.first = 0
             cont.last = 0
         end
@@ -73,14 +77,16 @@ end
 
 #pop()
 
-function Pop(cont::Container{T}) where T
+function Base.pop!(cont::Container{T}) where T
     
-    if !isEmpty(cont)
-        pop!(cont.vec)
+    if !isempty(cont)
+        popped = pop!(cont.vec)
         cont.first -= 1
         cont.size -= 1
     else
         error("Container is Empty.\n")
     end
-    return cont
+    return popped
 end
+
+end #module
