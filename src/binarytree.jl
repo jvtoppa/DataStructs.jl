606506a18e6bd5a0c64978fe9109a03236695791
jvtoppa <jvtoppa@gmail.com>
2026-02-07 05:40:16 -0300

    feat: added successor operation

--- src/binarytree.jl
+++ src/binarytree.jl
@@ -34,7 +34,7 @@ BinarySearchTree{T}(v::Vector{T}) where T = begin
     return bt
 end
 
-function isempty(bt::BinaryTree{T}) where T
+function Base.isempty(bt::BinaryTree{T}) where T
 
     return bt.root === nothing
 
@@ -42,12 +42,12 @@ end
 
 #length()
 
-length(tree::BinaryTree{T}) where T = tree.size
+Base.length(tree::BinaryTree{T}) where T = tree.size
 
 
 #clear()
 
-function clear!(tree::BinaryTree{T}) where T
+function Base.empty!(tree::BinaryTree{T}) where T
     tree.root = nothing
     tree.size = 0
 end
@@ -76,50 +76,77 @@ function print_tree_vertical(tree::BinarySearchTree{T}; level::Int=0) where T
     end
 end
 
-function insert_root(tree::BinarySearchTree{T}, treeHead::Union{NodeBST{T},Nothing}, key::T) where T
+function _insert!(tree::BinarySearchTree{T}, treeHead::Union{NodeBST{T},Nothing}, key::T) where T
     if treeHead === nothing
         tree.size += 1
         return NodeBST{T}(key, nothing, nothing)
     end
     
     if key < treeHead.elem
-        treeHead.left = insertW(tree, treeHead.left, key)
+        treeHead.left = _insert!(tree, treeHead.left, key)
     elseif key > treeHead.elem
-        treeHead.right = insertW(tree, treeHead.right, key)
+        treeHead.right = _insert!(tree, treeHead.right, key)
     end    
     return treeHead
 end
 
-function search_non_head(treeHead::Union{NodeBST{T}, Nothing}, key::T) where T
+function _search(treeHead::Union{NodeBST{T}, Nothing}, key::T) where T
     if treeHead === nothing
         return false
     end
 
     if key < treeHead.elem
-        return search(tree, treeHead.left, key)
+        return _search(treeHead.left, key)
     elseif key > treeHead.elem
-        return search(tree, treeHead.right, key)
+        return _search(treeHead.right, key)
     else
         return true
     end
 end
 
 function search(tree::BinarySearchTree{T}, key::T) where T #wrapper
-    return search_non_head(tree.root, key)
+    return _search(tree.root, key)
 end
 
+Base.in(key, tree::BinarySearchTree) = search(tree, key)
 
+function leftmostnode(node::Union{NodeBST{T}, Nothing}) where T
+    while node.left !== nothing
+        node = node.left
+    end
+    return node
+end
 
+function _successor(node::Union{NodeBST{T}, Nothing}, target::T) where T
+    curr = node
+    succ = nothing
+
+    while curr !== nothing
+        if target < curr.elem
+            succ = curr
+            curr = curr.left
+        else
+            curr = curr.right
+        end
+    end
+    return succ
+end
+
+function successor(tree::Union{BinarySearchTree{T}, Nothing}, target::T) where T
+    node = _successor(tree.root, target)
+    return node === nothing ? nothing : node.elemend
+end
 
 function Base.insert!(tree::BinarySearchTree{T}, key::T) where T
-    tree.root = insert_root(tree, tree.root, key)
+    tree.root = _insert!(tree, tree.root, key)
     return tree
 end
 
 function main()
     bt = BinarySearchTree{Int}([1,2,3,4,56,7,8,9,200,123,4])
     print_tree_vertical(bt)
-    print(isempty(bt))  
+    print(isempty(bt))
+    print(successor(bt, 56))  
 end
 
 main()
