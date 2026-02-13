5bfc63dbb62f3caf6ae65c39e6d2db6a5c6adb62
seixasr <rafaelseixas.figueiredo@gmail.com>
2026-02-13 15:32:55 -0300

    Changed functions namesand stacks on avl

--- src/DataStructs.jl
+++ src/DataStructs.jl
@@ -6,8 +6,7 @@ include("avl.jl")
 using .Avl
 
 export Container, Stack, Queue, Push, Pop, Peek, TopIdx, BottomIdx, SizeC, isEmpty, FindElement,
-RemoveNode, InsertAnyNode, NodeLL, LinkedList, AVLNode, AVLTree, avl_insert!, avl_delete!, avl_search,
-avl_inorder, avl_preorder, avl_postorder, avl_min, avl_max, avl_height, avl_isbalanced
+RemoveNode, InsertAnyNode, NodeLL, LinkedList, AVLNode, AVLTree, avl_insert!, avl_delete!, avl_search, avl_inorder, avl_preorder, avl_postorder, avl_min, avl_max, avl_height, avl_isbalanced
 import Base: show
 
 end
--- src/avl.jl
+++ src/avl.jl
@@ -1,7 +1,8 @@
 module Avl
 
-export AVLNode, AVLTree, avl_insert!, avl_delete!, avl_search, avl_inorder, avl_preorder,
-       avl_postorder, avl_min, avl_max, avl_height, avl_isbalanced
+export AVLNode, AVLTree, avl_insert!, avl_delete!, avl_search, avl_inorder, avl_preorder, avl_postorder, avl_min, avl_max, avl_height, avl_isbalanced
+
+import ..Containers: Stack, push!, pop!, isempty
 
 mutable struct AVLNode{T}
     key::T
@@ -19,44 +20,175 @@ end
 AVLTree{T}() where T = AVLTree{T}(nothing)
 AVLTree() = AVLTree{Any}(nothing)
 
-# Helpers
-height(n) = n === nothing ? 0 : n.height
-_update_height!(n::AVLNode) = (n.height = 1 + max(height(n.left), height(n.right)))
-balance_factor(n) = n === nothing ? 0 : height(n.left) - height(n.right)
+"""
+    avl_insert!(t::AVLTree{T}, key::T)
+Insert the element `key` into the AVL tree `t`. If the key already exists, the operation is a no-op.
+"""
+function avl_insert!(t::AVLTree{T}, key::T) where T
+    t.root = _insert_node(t.root, key)
+    return t
+end
 
-# Rotations
-function right_rotate(y::AVLNode{T}) where T
+"""
+    avl_delete!(t::AVLTree{T}, key::T)
+Remove the element `key` from the AVL tree `t`.
+"""
+function avl_delete!(t::AVLTree{T}, key::T) where T
+    t.root = _delete_node(t.root, key)
+    return t
+end
+
+"""
+    avl_search(t::AVLTree{T}, key::T)
+Search for the element `key` in the AVL tree `t`. Returns the node or `nothing`.
+"""
+function avl_search(t::AVLTree{T}, key::T) where T
+    return _search_node(t.root, key)
+end
+
+"""
+    avl_inorder(t::AVLTree{T})
+Returns a vector with the elements in order (inorder) using a stack.
+"""
+function avl_inorder(t::AVLTree{T}) where T
+    res = Vector{T}()
+    stack = Stack{AVLNode{T}}()
+    curr = t.root
+    while curr !== nothing || !isempty(stack)
+        while curr !== nothing
+            push!(stack, curr)
+            curr = curr.left
+        end
+        curr = pop!(stack)
+        push!(res, curr.key)
+        curr = curr.right
+    end
+    return res
+end
+
+"""
+    avl_preorder(t::AVLTree{T})
+Returns a vector with the elements in pre-order (preorder) using a stack.
+"""
+function avl_preorder(t::AVLTree{T}) where T
+    res = Vector{T}()
+    stack = Stack{AVLNode{T}}()
+    curr = t.root
+    if curr !== nothing
+        push!(stack, curr)
+    end
+    while !isempty(stack)
+        curr = pop!(stack)
+        push!(res, curr.key)
+        if curr.right !== nothing
+            push!(stack, curr.right)
+        end
+        if curr.left !== nothing
+            push!(stack, curr.left)
+        end
+    end
+    return res
+end
+
+"""
+    avl_postorder(t::AVLTree{T})
+Returns a vector with the elements in post-order (postorder) using a stack.
+"""
+function avl_postorder(t::AVLTree{T}) where T
+    res = Vector{T}()
+    stack = Stack{AVLNode{T}}()
+    last_visited = nothing
+    curr = t.root
+    while curr !== nothing || !isempty(stack)
+        if curr !== nothing
+            push!(stack, curr)
+            curr = curr.left
+        else
+            peek_node = stack.vec[stack.first]
+            if peek_node.right !== nothing && last_visited !== peek_node.right
+                curr = peek_node.right
+            else
+                curr = pop!(stack)
+                push!(res, curr.key)
+                last_visited = curr
+                curr = nothing
+            end
+        end
+    end
+    return res
+end
+
+"""
+    avl_min(t::AVLTree{T})
+Returns the minimum element of the AVL tree `t`.
+"""
+function avl_min(t::AVLTree{T}) where T
+    if t.root === nothing
+        return nothing
+    end
+    return _min_node(t.root).key
+end
+
+"""
+    avl_max(t::AVLTree{T})
+Returns the maximum element of the AVL tree `t`.
+"""
+function avl_max(t::AVLTree{T}) where T
+    if t.root === nothing
+        return nothing
+    end
+    node = t.root
+    while node.right !== nothing
+        node = node.right
+    end
+    return node.key
+end
+
+"""
+    avl_height(t::AVLTree)
+Returns the height of the AVL tree `t`.
+"""
+function avl_height(t::AVLTree)
+    return _height(t.root)
+end
+
+"""
+    avl_isbalanced(t::AVLTree)
+Returns `true` if the AVL tree `t` is balanced.
+"""
+function avl_isbalanced(t::AVLTree)
+    return _is_balanced_node(t.root)
+end
+
+# Private helpers
+_height(n) = n === nothing ? 0 : n.height
+_update_height!(n::AVLNode) = (n.height = 1 + max(_height(n.left), _height(n.right)))
+_balance_factor(n) = n === nothing ? 0 : _height(n.left) - _height(n.right)
+
+function _right_rotate(y::AVLNode{T}) where T
     x = y.left
     T2 = x.right
-
     x.right = y
     y.left = T2
-
     _update_height!(y)
     _update_height!(x)
-
     return x
 end
 
-function left_rotate(x::AVLNode{T}) where T
+function _left_rotate(x::AVLNode{T}) where T
     y = x.right
     T2 = y.left
-
     y.left = x
     x.right = T2
-
     _update_height!(x)
     _update_height!(y)
-
     return y
 end
 
-# Insertion (recursive)
 function _insert_node(n::Union{AVLNode{T}, Nothing}, key::T) where T
     if n === nothing
         return AVLNode(key)
     end
-
     if key < n.key
         n.left = _insert_node(n.left, key)
     elseif key > n.key
@@ -65,38 +197,25 @@ function _insert_node(n::Union{AVLNode{T}, Nothing}, key::T) where T
         # duplicate keys are ignored (no-op)
         return n
     end
-
     _update_height!(n)
-    bf = balance_factor(n)
-
-    # Left Left
+    bf = _balance_factor(n)
     if bf > 1 && key < n.left.key
-        return right_rotate(n)
+        return _right_rotate(n)
     end
-    # Right Right
     if bf < -1 && key > n.right.key
-        return left_rotate(n)
+        return _left_rotate(n)
     end
-    # Left Right
     if bf > 1 && key > n.left.key
-        n.left = left_rotate(n.left)
-        return right_rotate(n)
+        n.left = _left_rotate(n.left)
+        return _right_rotate(n)
     end
-    # Right Left
     if bf < -1 && key < n.right.key
-        n.right = right_rotate(n.right)
-        return left_rotate(n)
+        n.right = _right_rotate(n.right)
+        return _left_rotate(n)
     end
-
     return n
 end
 
-function avl_insert!(t::AVLTree{T}, key::T) where T
-    t.root = _insert_node(t.root, key)
-    return t
-end
-
-# Find minimum node in subtree
 function _min_node(n::AVLNode{T}) where T
     while n.left !== nothing
         n = n.left
@@ -104,12 +223,10 @@ function _min_node(n::AVLNode{T}) where T
     return n
 end
 
-# Deletion (recursive)
 function _delete_node(n::Union{AVLNode{T}, Nothing}, key::T) where T
     if n === nothing
         return nothing
     end
-
     if key < n.key
         n.left = _delete_node(n.left, key)
     elseif key > n.key
@@ -129,140 +246,47 @@ function _delete_node(n::Union{AVLNode{T}, Nothing}, key::T) where T
             n.right = _delete_node(n.right, succ.key)
         end
     end
-
     if n === nothing
         return nothing
     end
-
     _update_height!(n)
-    bf = balance_factor(n)
-
-    # Left Left
-    if bf > 1 && balance_factor(n.left) >= 0
-        return right_rotate(n)
+    bf = _balance_factor(n)
+    if bf > 1 && _balance_factor(n.left) >= 0
+        return _right_rotate(n)
     end
-    # Left Right
-    if bf > 1 && balance_factor(n.left) < 0
-        n.left = left_rotate(n.left)
-        return right_rotate(n)
+    if bf > 1 && _balance_factor(n.left) < 0
+        n.left = _left_rotate(n.left)
+        return _right_rotate(n)
     end
-    # Right Right
-    if bf < -1 && balance_factor(n.right) <= 0
-        return left_rotate(n)
+    if bf < -1 && _balance_factor(n.right) <= 0
+        return _left_rotate(n)
     end
-    # Right Left
-    if bf < -1 && balance_factor(n.right) > 0
-        n.right = right_rotate(n.right)
-        return left_rotate(n)
+    if bf < -1 && _balance_factor(n.right) > 0
+        n.right = _right_rotate(n.right)
+        return _left_rotate(n)
     end
-
     return n
 end
 
-function avl_delete!(t::AVLTree{T}, key::T) where T
-    t.root = _delete_node(t.root, key)
-    return t
-end
-
-# Search
-function _avl_search(n::Union{AVLNode{T}, Nothing}, key::T) where T
+function _search_node(n::Union{AVLNode{T}, Nothing}, key::T) where T
     if n === nothing
         return nothing
     end
     if key == n.key
         return n
     elseif key < n.key
-        return _avl_search(n.left, key)
+        return _search_node(n.left, key)
     else
-        return _avl_search(n.right, key)
+        return _search_node(n.right, key)
     end
 end
 
-function avl_search(t::AVLTree{T}, key::T) where T
-    return _avl_search(t.root, key)
-end
-
-# Traversals returning Vector{T}
-function _inorder(n::Union{AVLNode{T}, Nothing}, res::Vector{T}) where T
-    if n === nothing
-        return
-    end
-    _inorder(n.left, res)
-    push!(res, n.key)
-    _inorder(n.right, res)
-end
-
-function avl_inorder(t::AVLTree{T}) where T
-    res = Vector{T}()
-    _inorder(t.root, res)
-    return res
-end
-
-function _preorder(n::Union{AVLNode{T}, Nothing}, res::Vector{T}) where T
-    if n === nothing
-        return
-    end
-    push!(res, n.key)
-    _preorder(n.left, res)
-    _preorder(n.right, res)
-end
-
-function avl_preorder(t::AVLTree{T}) where T
-    res = Vector{T}()
-    _preorder(t.root, res)
-    return res
-end
-
-function _postorder(n::Union{AVLNode{T}, Nothing}, res::Vector{T}) where T
-    if n === nothing
-        return
-    end
-    _postorder(n.left, res)
-    _postorder(n.right, res)
-    push!(res, n.key)
-end
-
-function avl_postorder(t::AVLTree{T}) where T
-    res = Vector{T}()
-    _postorder(t.root, res)
-    return res
-end
-
-# Min / Max keys
-function avl_min(t::AVLTree{T}) where T
-    if t.root === nothing
-        return nothing
-    end
-    return _min_node(t.root).key
-end
-
-function avl_max(t::AVLTree{T}) where T
-    if t.root === nothing
-        return nothing
-    end
-    node = t.root
-    while node.right !== nothing
-        node = node.right
-    end
-    return node.key
-end
-
-# Height of tree
-function avl_height(t::AVLTree)
-    return height(t.root)
-end
-
-# Validate AVL property for every node
 function _is_balanced_node(n::Union{AVLNode, Nothing})
     if n === nothing
         return true
     end
-    bf = balance_factor(n)
+    bf = _balance_factor(n)
     return abs(bf) <= 1 && _is_balanced_node(n.left) && _is_balanced_node(n.right)
-end
-
-function avl_isbalanced(t::AVLTree)
-    return _is_balanced_node(t.root)
 end
 
 end # module Avl
